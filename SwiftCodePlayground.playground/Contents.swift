6cf1f1d1c28e15aa54e01848495976f0d84253df
Katie Chai <kchai@unc.edu>
2023-07-30 19:49:00 -0400

    swift functions

--- SwiftCodePlayground.playground/Contents.swift
+++ SwiftCodePlayground.playground/Contents.swift
@@ -14,3 +14,28 @@ print(myDouble)
 
 var myBool:Bool = false
 print(myBool)
+
+func sayHello(){
+    var greeting:String = "Hello, playground"
+    print(greeting)
+}
+sayHello()
+
+func sayHello(m:String){
+    print(m)
+}
+sayHello(m:"I can code functions!")
+
+func doAdd(a:Int, b:Int){
+    var sum = a + b
+    print(sum)
+}
+doAdd(a:2, b:3)
+
+func letAdd(_ a:Int, secondNum b:Int) -> Int {
+    var sum = a + b
+    return sum
+}
+
+let result = letAdd(2, secondNum:3)
+print(result)
