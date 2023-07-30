2e2ddbf4ba3c24f60db809f39916bd706e08714e
Katie Chai <kchai@unc.edu>
2023-07-30 19:38:47 -0400

    intro to swift

---
+++ SwiftCodePlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+print(greeting)
+
+greeting = "I can write Swift!"
+print(greeting)
+
+let myInt:Int = 5
+print(myInt)
+
+var myDouble:Double = 0.5
+print(myDouble)
+
+var myBool:Bool = false
+print(myBool)
