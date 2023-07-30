064c55b7a8a2462e20324526185354bc1c636bda
Katie Chai <kchai@unc.edu>
2023-07-30 17:35:26 -0400

    lesson 2 demo

---
+++ L2 Demo/L2 Demo/L2_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2_DemoApp.swift
+//  L2 Demo
+//
+//  Created by Katie Chai on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct L2_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
