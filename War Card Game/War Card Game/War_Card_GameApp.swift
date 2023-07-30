ae2a7c6b7e7b523b2f12db175cb0fccb83b6b8d5
Katie Chai <kchai@unc.edu>
2023-07-30 19:03:40 -0400

    game part 1

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Katie Chai on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
